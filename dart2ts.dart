e9d81ef1d8a3bafa7ddd3fd5cd62ae5b0ad1a950
Vittorio Ballestra <vittorio.ballestra@gmail.com>
2018-02-22 21:47:11 +0100

    adding some test

--- dart2ts.dart
+++ dart2ts.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 import 'package:build_runner/build_runner.dart';
 import 'package:dart2ts/dart2ts.dart';
 
 main() {
+  Directory dir = new Directory('.dart_tool');
+  if (dir.existsSync()) dir.deleteSync(recursive: true);
   build([
     new BuildAction(new Dart2TsBuilder(), 'html5', inputs: ['lib/**.dart']),
-  ]);
+  ], deleteFilesByDefault: true);
 }
